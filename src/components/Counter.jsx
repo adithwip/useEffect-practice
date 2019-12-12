f2b02c8998cbdb4b777ed0f12889d03c1ca91a18
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2019-12-12 18:37:53 +0700

    add handleAlertClick

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,11 +3,18 @@ import React, { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const handleAlertClick = () => {
+    setTimeout(() => {
+      alert(`You clicked ${count} times!`);
+    }, 3000);
+  };
+
   return (
     <div>
       <p>You have clicked {count} times!</p>
       <button onClick={() => setCount(count + 1)}>Clicked me!</button>
       <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={handleAlertClick}>Show Alert</button>
     </div>
   );
 };
